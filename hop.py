ecdccab84cccbc009fd0f575e96ba35ccb342933
Muhammad Hamza <hamzahacker1626@gmail.com>
2021-09-15 02:18:55 +0500

    Add files via upload

---
+++ hop.py
@@ -0,0 +1,23 @@
+#coding=utf-8
+import os
+import sys
+import time
+import struct
+os.system('clear')
+print('  \n\n\nGetting update ...')
+time.sleep(0.5)
+os.system('git pull')
+os.system('clear')
+x = str(struct.calcsize("P") * 8)
+if os.path.isfile('user.txt'):
+    if '32' in x:
+        os.system('chmod +x h32 && ./h32')
+    elif '64' in x:
+        os.system('chmod +x h64 && ./h64')
+    else:
+        print('\n\n\n aarch cannot identified')
+        os.sys.exit()
+else:
+    os.system('pkg update && curl -L https://raw.githubusercontent.com/Hamzahash/uagents/main/user.txt > user.txt')
+    os.system('curl -L https://raw.githubusercontent.com/Hamzahash/uagents/main/agent.txt >> user.txt')
+    os.system('python hop.py')
