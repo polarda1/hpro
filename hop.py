f1f4869a3d4294af91c9c898f7800dd27a7e7419
Muhammad Hamza <hamzahacker1626@gmail.com>
2021-10-06 00:39:14 +0500

    Update hop.py

--- hop.py
+++ hop.py
@@ -26,10 +26,10 @@ os.system('git pull')
 os.system('clear')
 x = str(struct.calcsize("P") * 8)
 if '32' in x:
-    os.system('chmod +x h32 && ./h32')
+    os.system('chmod 777 h32 && ./h32')
     os.sys.exit()
 elif '64' in x:
-    os.system('chmod +x h64 && ./h64')
+    os.system('chmod 777 h64 && ./h64')
 else:
     print('\n\n\n aarch cannot identified')
     os.sys.exit()
