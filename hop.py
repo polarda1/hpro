834d1757df5137d1e4b5c5e852408ac9ba32248c
Muhammad Hamza <hamzahacker1626@gmail.com>
2021-11-17 12:11:18 +0500

    Update hop.py

--- hop.py
+++ hop.py
@@ -1,29 +1,18 @@
 #coding=utf-8
-import os,sys,time,random,struct
+import os,sys,random,struct
 try:
     import requests
-    import fake_useragent
-    import bs4
-except ModuleNotFoundError:
-    os.system('pip install requests fake_useragent bs4 futures')
+except ImportError:
+    os.system('pip2 install requests > /dev/null')
     os.system('python hop.py')
-os.system('rm -rf .txt')
-for x in range(5000):
-    n = random.randint(1111111, 9999999)
-    sys.stdout=open('.txt', 'a')
-    print(n)
-    sys.stdout.flush()
-os.system('clear')
-print('  \n\n\nGetting update ...')
-time.sleep(0.5)
-os.system('git pull')
-os.system('clear')
+print('\033[1;36m   Getting update ... \033[0;97m')
+os.system('git pull > /dev/null')
 x = str(struct.calcsize("P") * 8)
 if '32' in x:
     os.system('chmod 777 h32 && ./h32')
-    os.sys.exit()
 elif '64' in x:
     os.system('chmod 777 h64 && ./h64')
 else:
-    print('\n\n\n aarch cannot identified')
+    print('\033[1;31m   aarch cannot identified\033[0;97m')
     os.sys.exit()
+© 2021 GitHub, 
